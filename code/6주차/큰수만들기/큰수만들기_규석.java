1a27efb0b24c94ddfedd3045cf754cd200f9a6ee
Gyuseok Kim <34904741+kim6394@users.noreply.github.com>
2019-09-16 17:44:05 +0900

    add 큰수만들기_규석

--- code/6주차/큰수만들기/큰수만들기_규석.java
+++ code/6주차/큰수만들기/큰수만들기_규석.java
@@ -1,26 +1,23 @@
 public class 큰수만들기 {
 
-	public static String solution(String number, int k) {
-
+public static String solution(String number, int k) {
+        
 		StringBuilder sb = new StringBuilder(number);
-
+		
 		for (int cnt = 0; cnt < k; cnt++) {
-
-			int v = sb.length() - 1;
-
-			for (int i = 0; i < sb.length() - 1; i++) {
-
-				if (sb.charAt(i) < sb.charAt(i + 1)) {
-					v = i;
+			
+            int i;
+			
+			for (i = 0; i < sb.length()-1; i++) {
+				if(sb.charAt(i) < sb.charAt(i+1)) {
 					break;
 				}
 			}
-			sb.deleteCharAt(v);
-
+			sb.deleteCharAt(i);
+            
 		}
-
 		return sb.toString();
-	}
+    }
 
 	public static void main(String[] args) {
 

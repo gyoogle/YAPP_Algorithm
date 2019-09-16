c1406fa91c937b185e50dded2b4c7f9365a517cd
scd7896 <scd1212@naver.com>
2019-09-16 18:21:55 +0900

    큰수만들기

---
+++ code/6주차/큰수만들기/태경.js
@@ -0,0 +1,38 @@
+function solution(number, k) {
+    var answer = ''
+    let arr = number.split('').map(e => parseInt(e))
+    
+    for(let i = 0 ; i < k ; i++){
+        let j
+        for( j= 0; j<arr.length-1; j++ ){
+            if(arr[j] < arr[j+1]){
+                break;
+            }
+        }
+        arr.splice(j,1)
+        
+    }
+    for(let i = 0 ; i< arr.length; i++){
+        answer+=arr[i]
+    }
+    return answer;
+}
+
+
+function solution(number, k) {
+    const answer = []
+    let head = 0
+    let del = k
+
+    answer.push(number[head++])
+    while(answer.length < number.length - k || head < number.length) {
+        if(del && answer[answer.length-1] < number[head]) {
+            answer.pop()
+            del--
+            continue
+        }
+        answer.push(number[head++])
+    }
+
+    return answer.slice(0, number.length - k).join('')
+}

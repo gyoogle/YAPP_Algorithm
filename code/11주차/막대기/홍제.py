0a146f4fbb6a07bbfe79d5dda6b08039b9bdfb48
chohongjae <bjq913@naver.com>
2019-10-28 19:00:53 +0900

    홍제

---
+++ code/11주차/막대기/홍제.py
@@ -0,0 +1,14 @@
+def solution(X):
+    a = [64]
+    while sum(a) > X:
+        b = a.pop()
+        a.append(b // 2)
+        a.append(b // 2)
+        if sum(a[:-1]) >= X:
+            a.pop()
+        elif sum(a[:-1]) == X:
+            break
+    return len(a)
+
+if __name__ == "__main__":
+    print(solution(23))

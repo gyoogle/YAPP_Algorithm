5ad848c12f66cafd45ee6b318015a75209fad243
scd7896 <scd1212@naver.com>
2020-03-27 17:58:23 +0900

    태경 정렬

---
+++ code/23주차/markAndToys/태경.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the maximumToys function below.
+function maximumToys(prices, k) {
+    let money = k;
+    let cnt = 0;
+    prices.sort((a,b)=> a-b);
+    for(let i = 0; i < prices.length; i++){
+        if(prices[i] <= money){
+            money -= prices[i];
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nk = readLine().split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const prices = readLine().split(' ').map(pricesTemp => parseInt(pricesTemp, 10));
+
+    let result = maximumToys(prices, k);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
